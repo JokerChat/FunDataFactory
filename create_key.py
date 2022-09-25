11c144c870ad25a8051c835d72c9eccf8fbc5015
JokerChat <664616581@qq.com>
2022-09-25 09:23:44 +0800

    更新RSA私钥生成

--- create_key.py
+++ create_key.py
@@ -4,17 +4,20 @@
 # @File : create_key.py
 
 import paramiko
-from app.commons.settings.config import FilePath
-
+import os
 
 def generate_ssh_key() -> None:
+    key_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'app/commons/settings/keys')
+    if not os.path.isdir(key_path): os.mkdir(key_path)
+    rsa_pri_key = os.path.join(key_path, 'rsa_pri_key')
+    ras_pub_key = os.path.join(key_path, 'rsa_pub_key')
     key = paramiko.RSAKey.generate(4096)
     # 私钥生成
-    key.write_private_key_file(FilePath.RSA_PRI_KEY)
+    key.write_private_key_file(rsa_pri_key)
     # 公钥生成
-    rsa_pub_key = f"{key.get_name()} {key.get_base64()} FunDataFactory"
-    with open(FilePath.RSA_PUB_KEY, 'w') as f:
-        f.write(rsa_pub_key)
+    pub_key = f"{key.get_name()} {key.get_base64()} FunDataFactory"
+    with open(ras_pub_key, 'w') as f:
+        f.write(pub_key)
 
 if __name__ == '__main__':
     generate_ssh_key()
